c0d084c7d2899698ae92e904e6b7ef61539f02e0
wrightkhlebisol <calebogundiya@gmail.com>
2024-03-05 14:19:08 +0000

    no params reassign

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (const idx of array) {
     const value = array[idx];
+    // eslint-disable-next-line no-param-reassign
     array[idx] = appendString + value;
   }
 
