24a9b8fbac0928bcb8aa9c18ca43f8af01d1917d
wrightkhlebisol <calebogundiya@gmail.com>
2024-03-05 11:43:29 +0000

    objects on the fly

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,5 +1,5 @@
 export default function createEmployeesObject(departmentName, employees) {
-  const obj = {};
-  obj[departmentName] = employees;
-  return obj;
+  return {
+    [departmentName]: employees,
+  };
 }
