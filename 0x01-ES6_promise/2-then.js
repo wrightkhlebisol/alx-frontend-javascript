6c2c6184ee32c41d2c03fdd951696f764d121e71
wrightkhlebisol <calebogundiya@gmail.com>
2024-03-06 21:41:05 +0000

    handle finally

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    console.log('Got a response from the API');
-    return {
+  promise
+    .then(() => ({
       status: 200,
       body: 'success',
-    };
-  }).catch(() => new Error());
+    }))
+    .catch(() => new Error())
+    .finally(console.log('Got a response from the API'));
 }
