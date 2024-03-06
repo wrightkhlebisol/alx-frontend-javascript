79d9d9ceb971b58d3c0169e2efeb91a18ca14779
wrightkhlebisol <calebogundiya@gmail.com>
2024-03-06 23:49:54 +0000

    final?

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,5 +2,6 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then((res) => console.log(res));
+  Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => console.log(res));
 }
