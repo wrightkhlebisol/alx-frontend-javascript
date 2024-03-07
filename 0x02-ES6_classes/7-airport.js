d2ec30e25b59732db1fd79ade9c69a4d527d8282
wrightkhlebisol <calebogundiya@gmail.com>
2024-03-07 22:18:42 +0000

    override tostring

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-underscore-dangle */
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string') throw new TypeError('Name must be a string');
+    if (typeof code !== 'string') throw new TypeError('Code must be a string');
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  set code(newCode) {
+    this._code = newCode;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
