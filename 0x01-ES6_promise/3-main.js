fa6c9324382e5c55b1047be75247d4884e3d36aa
wrightkhlebisol <calebogundiya@gmail.com>
2024-03-07 00:23:26 +0000

    main files

---
+++ 0x01-ES6_promise/3-main.js
@@ -0,0 +1,5 @@
+import handleProfileSignup from "./3-all";
+
+handleProfileSignup();
+
+
---
+++ 0x01-ES6_promise/6-main.js
@@ -0,0 +1,3 @@
+import handleProfileSignup from './6-final-user';
+
+handleProfileSignup('Bob', 'Dylan', 'bob_dylan.jpg').then((res) => console.log(res));
