42627cc217604a05ee0511ca0051c7756b3d4888
wrightkhlebisol <calebogundiya@gmail.com>
2024-03-07 16:31:41 +0000

    add setters and getters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -2,7 +2,7 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') throw new TypeError('Name must be a string');
-    if (typeof length !== 'number') throw new TypeError('Length must be a string');
+    if (typeof length !== 'number') throw new TypeError('Length must be a number');
     if (typeof students !== 'object') throw new TypeError('Students must be an object');
 
     this._name = name;
