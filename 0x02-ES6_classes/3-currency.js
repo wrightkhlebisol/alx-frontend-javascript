35372ca5f3b959c4916ce0c4548979a24e80365c
wrightkhlebisol <calebogundiya@gmail.com>
2024-03-07 16:29:26 +0000

    print currency

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-underscore-dangle */
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string') throw new TypeError('Code must be a string');
+    if (typeof name !== 'string') throw new TypeError('Name must be a string');
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(code) {
+    this._code = code;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  displayCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
