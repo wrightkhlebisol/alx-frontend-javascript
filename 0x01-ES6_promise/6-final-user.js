3a646da798f2cb7eaea7bd1a92189d87f95053de
wrightkhlebisol <calebogundiya@gmail.com>
2024-03-07 12:18:37 +0000

    iterate reult

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,18 +5,13 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((res) => {
-    const newRes = [];
-    res.forEach((re) => {
-      const reObj = {};
-      if (re.value instanceof Error) {
-        reObj.value = re.value.message;
-      } else {
-        reObj.value = re.value;
-      }
-      reObj.status = re.status;
-      newRes.push(reObj);
-    });
-    return newRes;
-  });
+  ]).then((res) => res.map((re) => {
+    if (re.status === 'rejected') {
+      return {
+        status: re.status,
+        value: re.reason.message,
+      };
+    }
+    return re;
+  }));
 }
