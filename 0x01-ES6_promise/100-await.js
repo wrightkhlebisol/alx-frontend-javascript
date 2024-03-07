0436023429977c320089aee0ff5a9aa282a4e43b
wrightkhlebisol <calebogundiya@gmail.com>
2024-03-07 01:10:34 +0000

    async function

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -3,11 +3,8 @@ import { uploadPhoto, createUser } from './utils';
 export default async function asyncUploadUser() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((res) => ({
-      photo: res[0].body,
-      user: {
-        firstName: res[1].firstName,
-        lastName: res[1].lastName,
-      },
+      photo: res[0],
+      user: res[1],
     }))
     .catch(() => ({ photo: null, user: null }));
 }
