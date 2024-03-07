6faa771365d0748a1a9138f43f6f16dd2457a9fd
wrightkhlebisol <calebogundiya@gmail.com>
2024-03-07 00:26:04 +0000

    return promise all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then(
       (res) => console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`),
     )
