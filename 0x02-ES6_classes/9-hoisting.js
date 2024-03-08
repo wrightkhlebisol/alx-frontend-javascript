22474b201a0c7dcdade624902736479ea6ba3546
wrightkhlebisol <calebogundiya@gmail.com>
2024-03-08 21:30:58 +0000

    Fix: have class in same file

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,6 +1,4 @@
 /* eslint-disable no-underscore-dangle */
-import StudentHolberton from './StudentHolberton';
-
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -13,6 +11,26 @@ export class HolbertonClass {
 
   get location() {
     return this._location;
+  }
+}
+
+export class StudentHolberton {
+  constructor(firstName, lastName, holbertonClass) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._holbertonClass = holbertonClass;
+  }
+
+  get fullName() {
+    return `${this._firstName} ${this._lastName}`;
+  }
+
+  get holbertonClass() {
+    return this._holbertonClass;
+  }
+
+  get fullStudentDescription() {
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
 
