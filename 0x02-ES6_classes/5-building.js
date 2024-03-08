29509a4ec58a89164c97f9458584dea4e72b60d9
wrightkhlebisol <calebogundiya@gmail.com>
2024-03-08 01:26:17 +0000

    throw abstract error

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 export default class Building {
   constructor(sqft) {
-    if (typeof amount !== 'number') throw new TypeError('Amount should be an integer');
+    if (typeof sqft !== 'number') throw new TypeError('Amount should be an integer');
     this._sqft = sqft;
   }
 
