5aec64193379e10d84e018de6a6ba2bda19266be
wrightkhlebisol <calebogundiya@gmail.com>
2024-03-08 21:33:34 +0000

    Fix: lint errors

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-underscore-dangle */
 export default class Car {
   constructor(brand, motor, color) {
-		if (typeof brand !== 'string') throw new TypeError('Brand must be a string');
+    if (typeof brand !== 'string') throw new TypeError('Brand must be a string');
     if (typeof motor !== 'string') throw new TypeError('Motor must be a string');
     if (typeof color !== 'string') throw new TypeError('Color must be a string');
-	
-		this._brand = brand;
+
+    this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
