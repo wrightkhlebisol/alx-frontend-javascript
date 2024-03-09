04b9596681fc1f4585f9fb592120592afaed9959
wrightkhlebisol <calebogundiya@gmail.com>
2024-03-09 01:08:02 +0000

    clone car

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,16 +1,12 @@
 /* eslint-disable no-underscore-dangle */
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof brand !== 'string') throw new TypeError('Brand must be a string');
-    if (typeof motor !== 'string') throw new TypeError('Motor must be a string');
-    if (typeof color !== 'string') throw new TypeError('Color must be a string');
-
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
   cloneCar() {
-    return this;
+    return new this.constructor();
   }
 }
