1ca8faf66816127d754013d500709f4bb5866aef
wrightkhlebisol <calebogundiya@gmail.com>
2024-03-09 00:42:10 +0000

    iterate rport objects

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,3 +1,11 @@
 export default function createIteratorObject(report) {
+  const iteratorArr = [];
 
+  const { allEmployees } = report;
+  // eslint-disable-next-line guard-for-in
+  for (const dept in allEmployees) {
+    iteratorArr.push(...allEmployees[dept]);
+  }
+
+  return iteratorArr;
 }
