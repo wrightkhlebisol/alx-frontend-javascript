0be1bdb6d29a7d608cb0be73ec5a3521727c3a06
wrightkhlebisol <calebogundiya@gmail.com>
2024-03-12 21:41:43 +0000

    reduce ids sum

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,7 @@
+export default function getStudentIdsSum(studentsList) {
+  if (Array.isArray(studentsList)) {
+    return studentsList.reduce((acc, cur) => acc + cur.id, 0);
+  }
+
+  return [];
+}
