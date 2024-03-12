d3b2a5040824bdee4552ee53f737f144a37534e1
wrightkhlebisol <calebogundiya@gmail.com>
2024-03-12 21:09:25 +0000

    get student lists

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,19 @@
+export default function getListStudents() {
+  return [
+    {
+      id: 1,
+      firsName: 'Guillaume',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firsName: 'James',
+      location: 'San Francisco',
+    },
+    {
+      id: 5,
+      firsName: 'Serena',
+      location: 'San Francisco',
+    },
+  ];
+}
