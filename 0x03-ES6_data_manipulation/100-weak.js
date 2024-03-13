b0d8258499a514eaccef2d3624158e633dc2a7e1
wrightkhlebisol <calebogundiya@gmail.com>
2024-03-13 11:16:54 +0000

    fix: incrment before throw

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,13 +1,10 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  if (weakMap.has(endpoint)) {
-    const queryTime = weakMap.get(endpoint);
-    if (queryTime >= 5) {
-      throw new Error('Endpoint load is high');
-    }
-    weakMap.set(endpoint, queryTime + 1);
-  } else {
-    weakMap.set(endpoint, 1);
+  const queryTime = weakMap.get(endpoint) || 0;
+  const newTime = queryTime + 1;
+  weakMap.set(endpoint, newTime);
+  if (newTime >= 5) {
+    throw new Error('Endpoint load is high');
   }
 }
