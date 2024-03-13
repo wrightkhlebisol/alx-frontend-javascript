d32695f8078fc19c5ddf867adcc727a7392fd0a1
wrightkhlebisol <calebogundiya@gmail.com>
2024-03-13 01:04:07 +0000

    split set member an djoin

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+export default function cleanSet(set, startString) {
+  if (typeof set === 'object' && typeof startString === 'string' && startString.length > 0) {
+    const newArr = [];
+    set.forEach((val) => {
+      const split1 = val.split(startString)[1];
+      if (split1) {
+        newArr.push(split1);
+      }
+    });
+    return newArr.join('-');
+  }
+  return '';
+}
