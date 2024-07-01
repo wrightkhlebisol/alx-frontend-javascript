9a4c641d81af5bfbf0bbea47320c024225c5ca3f
wrightkhlebisol <calebogundiya@gmail.com>
2024-07-01 22:11:43 +0100

    handle reduce for empty arrays

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -2,6 +2,5 @@ export default function getStudentIdsSum(studentsList) {
   if (Array.isArray(studentsList)) {
     return studentsList.reduce((acc, cur) => acc + cur.id, 0);
   }
-
-  return [];
+  return 0;
 }
